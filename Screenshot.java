de97ec5817603786b6f9a5305b7e11fdd7a2ec4d
José Manuel Echeverri P <palacio.90@hotmail.com>
2020-03-12 14:31:22 -0500

    Create Screenshot.java

---
+++ Screenshot.java
@@ -0,0 +1,9 @@
+public class Screenshot {
+
+    public static void takeSnapShot(WebDriver webdriver, String fileWithPath) throws Exception {
+        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        File DestFile = new File(fileWithPath);
+        FileUtils.copyFile(SrcFile, DestFile);
+    }
+}
